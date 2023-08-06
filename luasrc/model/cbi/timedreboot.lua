8aa305f0acaf4e54e05ca231ae2548b1755862a7
Hilman Maulana <hilman0.0maulana@gmail.com>
2023-08-06 12:59:40 +0700

    Update timedreboot.lua
    Add source code and replace TypedSection to NamedSection

--- luasrc/model/cbi/timedreboot.lua
+++ luasrc/model/cbi/timedreboot.lua
@@ -1,24 +1,24 @@
 require("luci.sys")
 
-m=Map("timedreboot",translate("Timed Reboot"),translate("Timed Reboot Setting. <a href=\"https://github.com/kongfl888/luci-app-timedreboot\">Github</a>"))
+m = Map("timedreboot",translate("Timed Reboot"),translate("Timed Reboot Setting.") .. [[<br/><br/><a href="https://github.com/kongfl888/luci-app-timedreboot" target="_blank">Powered by kongfl888</a>]])
 
-s=m:section(TypedSection,"main","")
-s.addremove=false
-s.anonymous=true
+s = m:section(NamedSection,"config","timedreboot")
+s.addremove = false
+s.anonymous = true
 
-enable=s:option(Flag,"enable",translate("Enable"))
+enable = s:option(Flag,"enable",translate("Enable"))
 enable.rmempty = false
 enable.default=0
 
-hour=s:option(Value,"hour",translate("Hour"))
+hour = s:option(Value,"hour",translate("Hour"))
 hour.datatype = "range(0,23)"
 hour.rmempty = false
 
-minute=s:option(Value,"minute",translate("Minute"))
+minute = s:option(Value,"minute",translate("Minute"))
 minute.datatype = "range(0,59)"
 minute.rmempty = false
 
-week=s:option(ListValue,"week",translate("Week Day"))
+week = s:option(ListValue,"week",translate("Week Day"))
 week:value(7,translate("Everyday"))
 week:value(1,translate("Monday"))
 week:value(2,translate("Tuesday"))
@@ -28,16 +28,16 @@ week:value(5,translate("Friday"))
 week:value(6,translate("Saturday"))
 week:value(0,translate("Sunday"))
 week:value(8,translate("Someday"))
-week.default=6
+week.default = 6
 
 day=s:option(Value,"sday",translate("Days of month"),translate("You also can use like this: 2,8,16 or 5-15."))
-day.default=15
+day.default = 15
 day.rmempty = true
 day:depends("week",8)
 
 once=s:option(Flag,"once",translate("Only once"))
 once.rmempty = false
-once.default=0
+once.default = 0
 
 local apply =luci.http.formvalue("cbi.apply")
 if apply then
